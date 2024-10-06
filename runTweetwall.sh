2cd626d63560d4c38275f17aa13cb67398680e7a
Martin Klähn <mklaehn@users.noreply.github.com>
2024-10-06 17:53:06 +0200

    runTweetwall with java platform 23 (#33)

--- runTweetwall.sh
+++ runTweetwall.sh
@@ -6,6 +6,9 @@ type=${1:?type was not provided}
 # Ensure (for the purposes of this script) that LANG is set to en_US
 export LANG=en_US.UTF-8
 
+# Set Java Platform to 23
+export JAVA_PLATFORM_VERSION=23
+
 # Update the git repos
 echo "# Pull the latest changes for TweetwallFX"
 test -d ../TweetwallFX \
